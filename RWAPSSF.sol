5f4a3824fc22a7f401bc197c075d54b6cd7101b0
Kittichai <100368005+Ioarsh@users.noreply.github.com>
2024-02-13 03:09:37 +0700

    Add timestamp and fix choice

--- RWAPSSF.sol
+++ RWAPSSF.sol
@@ -3,17 +3,20 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "./CommitReveal.sol";
 
-contract RWAPSSF is CommitReveal {
+contract RWAPSSF {
     struct Player {
-        uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
+        uint choice; // 0 - Rock, 1 - Fire , 2 - Scissors, 3 - Sponge, 4 - Paper, 5 - Air, 6 - Water, 7 - Undefined
+        uint timestamp;
         address addr;
+        bool foul;
     }
-    uint numPlayer = 0;
-    uint reward = 0;
+    uint internal  numPlayer = 0;
+    uint internal reward = 0;
     mapping (uint => Player) private player;
     mapping (address => uint) private PlayerIndex;
-    uint numInput = 0;
-    uint timelimit = 10 minutes;
+    uint internal numInput = 0;
+    uint internal choose_timelimit = 5 minutes;
+    uint internal waiting_timelimit = 10 minutes;
 
 
     function GameStatus() public view returns(uint Player_now, uint prize, uint Choice_now) {
@@ -27,16 +30,30 @@ contract RWAPSSF is CommitReveal {
         for(uint i=0;i<2;i++){
             PlayerIndex[player[i].addr] = 0;
             player[i].addr = address(0);
-            player[i].choice = 3;
+            player[i].choice = 7;
+            player[i].timestamp = block.timestamp;
+            player[i].foul = false;
         }
     }
-
+    
     function quit() public payable {
         uint idx = PlayerIndex[msg.sender];
-        require(numPlayer==1 || (numPlayer==2 && numInput!=2));
-
-        if(numPlayer==1){
+        require(numPlayer == 1 || numInput < 2);
+        if (numPlayer == 1) {
             payable(player[idx].addr).transfer(reward);
+        } else {
+            payable(player[0].addr).transfer(reward/2);
+            payable(player[1].addr).transfer(reward/2);
+        }
+        reset();
+    }
+
+    function terminate() private {
+        if(player[0].foul==true){
+            payable(player[1].addr).transfer(reward);
+        }
+        else if(player[1].foul==true){
+            payable(player[0].addr).transfer(reward);
         }
         else{
             payable(player[0].addr).transfer(reward/2);
@@ -50,16 +67,24 @@ contract RWAPSSF is CommitReveal {
         require(msg.value == 1 ether);
         reward += msg.value;
         player[numPlayer].addr = msg.sender;
-        player[numPlayer].choice = 3;
+        player[numPlayer].choice = 7;
+        player[numPlayer].foul = false;
         PlayerIndex[msg.sender] = numPlayer;
         numPlayer++;
+        if (numPlayer == 2) {
+            player[0].timestamp = block.timestamp;
+            player[1].timestamp = block.timestamp;
+        }
     }
 
-    function input(uint choice) public  {
+    function input(uint choice) public{
         uint idx = PlayerIndex[msg.sender];
         require(numPlayer == 2);
         require(msg.sender == player[idx].addr);
-        require(choice == 0 || choice == 1 || choice == 2);
+        require(choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5 || choice == 6);
+        if(block.timestamp > player[idx].timestamp + choose_timelimit){
+            player[idx].foul = true;
+        }
         player[idx].choice = choice;
         numInput++;
         if (numInput == 2) {
@@ -72,6 +97,10 @@ contract RWAPSSF is CommitReveal {
         uint p1Choice = player[1].choice;
         address payable account0 = payable(player[0].addr);
         address payable account1 = payable(player[1].addr);
+        if(player[0].foul == true || player[1].foul == true){
+            terminate();
+            return;
+        }
         if (((p0Choice + 1) % 7 == p1Choice) || ((p0Choice + 2) % 7 == p1Choice) || ((p0Choice + 3) % 7 == p1Choice)) {
             // to pay player[1]
             account1.transfer(reward);
